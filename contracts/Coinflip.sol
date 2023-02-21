38a340115d07648e6500cad03367c53df9719ccd
vicsnet <adesanmiadebisi@gmail.com>
2023-02-21 16:51:59 +0100

    Generating Random

--- contracts/Coinflip.sol
+++ contracts/Coinflip.sol
@@ -20,7 +20,7 @@ contract CoinFlip is VRFV2WrapperConsumerBase{
         HEAD,
         TAILS
     }
-    mapping(uint256 =>CoinFlipStatus) public statuses;
+    mapping(uint256 => CoinFlipStatus) public statuses;
     address constant  linkAddress = 	0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
     // address constant vrfWrapperAddress =
     address constant vrfCordinatorAddress = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
